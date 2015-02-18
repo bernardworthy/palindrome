e52ede6cbbe5cb96e6e5485a7bd6b7af1c96387d
Bernard <worthybernard@gmail.com>
2015-02-18 12:39:04 -0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function isPalindrome(str) {
+  var frontArr = str.split('');
+  var frontStr = frontArr.join('');
+  var backArr = [];
+
+  for (i = frontArr.length - 1; i >= 0; --i) {
+    backArr.push(frontArr[i]);
+  }
+
+  var backStr = backArr.join('');
+
+  if (backStr === frontStr) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
+}
+
+isPalindrome('hannah');
